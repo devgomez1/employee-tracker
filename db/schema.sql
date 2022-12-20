b5f1a592166394eaf5a7b91a0ea632b5669e2150
devin <devinelliotgomez@gmail.com>
2022-12-20 15:58:34 -0600

    dropped all tables if they have existed and created my first table

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,12 @@ DROP DATABASE IF EXISTS companyTracker_db;
 CREATE DATABASE companyTracker_db;
 
 USE companyTracker_db;
+
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
