457cf81b75d1410422d132de53c01cc202374afa
devin <devinelliotgomez@gmail.com>
2022-12-21 19:21:10 -0600

    added first prompt to the server.js

--- server.js
+++ server.js
@@ -1,4 +1,26 @@
 
 const inquirer = require('inquirer');
-const db = require('./db/mysql')
+const sql = require('./db/mysql')
 const consTable = require('console.table')
+
+sql.connect(function (error) {
+    if (error) throw error;
+    startDatabase();
+})
+
+function startDatabase() {
+    inquirer.prompt({
+        type: 'list',
+        message: 'WELCOME TO THE COMPANY TRACKER',
+        name: 'prompt',
+        choices: [
+            'VIEW DEPARTMENTS',
+            'VIEW ROLES',
+            'VIEW EMPLOYEES',
+            'ADD A DEPARTMENT',
+            'ADD A ROLE',
+            'ADD AN EMPLOYEE',
+            'UPDATE EMPLOYEE ROLE',
+        ]
+    })
+}
