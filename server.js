c8498010c2392cb71baed980172248586b4b2455
devin <devinelliotgomez@gmail.com>
2022-12-22 17:06:02 -0600

    got the first function to work displaying all the different departments with their respective ids

--- server.js
+++ server.js
@@ -8,23 +8,44 @@ sql.connect(function (error) {
     startDatabase();
 })
 
-function startDatabase() {
-    inquirer.prompt({
+async function startDatabase() {
+   const answers = await inquirer.prompt([
+    {
         type: 'list',
         message: 'WELCOME TO THE COMPANY TRACKER',
         name: 'prompt',
-        choices: [
-            'VIEW DEPARTMENTS',
-            'VIEW ROLES',
-            'VIEW EMPLOYEES',
-            'ADD A DEPARTMENT',
-            'ADD A ROLE',
-            'ADD AN EMPLOYEE'
-        ]
+        choices: ['VIEW DEPARTMENTS', 'VIEW ROLES', 'VIEW EMPLOYEES', 'ADD A DEPARTMENT', 'ADD A ROLE', 'ADD AN EMPLOYEE', 'FINISHED']
         
-    })
-}
-   
+    }
+]);
+
+    switch (answers.prompt) {
+        case 'VIEW DEPARTMENTS':
+            console.log('VIEWING ALL DEPARTMENTS');
+            displayDepartments();
+            break;
+        case 'VIEW ROLES': 
+            console.log('VIEWING ALL ROLES');
+            break;
+        case 'VIEW EMPLOYEES': 
+            console.log('VIEWING ALL EMPLOYEES');
+            break;
+        case 'ADD A DEPARTMENT': 
+            console.log('ADDING DEPARTMENT');
+            break;
+        case 'ADD A ROLE': 
+            console.log('ADDING ROLE');
+            break;
+        case 'ADD AN EMPLOYEE': 
+            console.log('ADDING EMPLOYEE');
+            break;
+        case 'FINISHED': 
+            console.log('ENDING DATABASE');
+            break;
+    }   
+};
+
+
 
 function displayDepartments() {
     sql.query("SELECT * FROM departments", function (error, res) {
@@ -32,5 +53,3 @@ function displayDepartments() {
         startDatabase();
     });
 };
-
-displayDepartments();
